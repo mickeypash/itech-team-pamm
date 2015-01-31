3e9251b16a64233639a9a1c133adaef309dc1b8f
Mickey Pashov <pashov.m@gmail.com>
2015-01-31 19:12:43 +0000

    Added medium editor demoe and played around with Generic views

--- pamm/mango/views.py
+++ pamm/mango/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
+# from django.views.generic import TemplateView
 
 # Create your views here.
 def index(request):
 	context_dict = {'boldmessage': "I am bold font from the context"}
 	return render(request, 'mango/index.html', context_dict)
 
-# def detail(request, question_id):
-#     try:
-#         question = Question.objects.get(pk=question_id)
-#     except Question.DoesNotExist:
-#         raise Http404("Question does not exist")
-#     return render(request, 'polls/detail.html', {'question': question})
+def demo(request):
+	return render(request, 'mango/demo.html')
+
+def hello(request):
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    return render(request, 'mango/index.html', context_dict)
+#
+# class AboutView(TemplateView):
+#     template_name = "demo.html"
+#
+# class IndexView(TemplateView):
+#     template_name = "index.html"
+
+# 404 Example
+# from django.shortcuts import get_object_or_404
+#
+# def my_view(request):
+#     my_object = get_object_or_404(MyModel, pk=1)
+
+#
