74613a20e2af82a3467cf90d2ae208f82951a49f
Mickey Pashov <pashov.m@gmail.com>
2015-02-26 20:58:10 +0000

    Copied add note and add tag views from @meras 's app noted

--- pamm/notes/views.py
+++ pamm/notes/views.py
@@ -1,6 +1,54 @@
 from django.shortcuts import render
 
-# Create your views here.
+# View of landing page
 def index(request):
 	context_dict = {'boldmessage': "the start of a beautiful relationship"}
 	return render(request, 'notes/index.html', context_dict)
+
+#@user_passes_test(superuser_only, login_url="/")
+def add_note(request):
+    id = request.GET.get('id', None)
+    if id is not None:
+        note = get_object_or_404(Note, id=id)
+    else:
+        note = None
+
+    if request.method =='POST':
+        if request.POST.get('control') == 'delete':
+            note.delete()
+            messages.add_message(request, messages.INFO, "Note deleted")
+            return HttpResponseRedirect(reverse('notes:index'))
+
+        form = NoteForm(request.POST, instance=note)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.INFO, "Note added")
+            return HttpResponseRedirect(reverse('notes:index'))
+    else:
+        form = NoteForm(instance=note)
+
+    return render(request, 'notes/addnote.html', {'form':form, 'note':note})
+
+#@user_passes_test(superuser_only, login_url="/")
+def add_tag(request):
+    id = request.GET.get('id', None)
+    if id is not None:
+        tag = get_object_or_404(Tag, id=id)
+    else:
+        tag = None
+    
+    if request.method == 'POST':
+        if request.POST.get('control') == 'delete':
+            tag.delete()
+            messages.add_message(request, messages.INFO, 'Tag deleted')
+            return HttpResponseRedirect(reverse('notes:index'))
+        form = TagForm(request.POST, instance=tag)
+        if form.is_valid():
+            t = form.save(commit=False)
+            t.slug = slugify(t.label)
+            t.save()
+            messages.add_message(request,messages.INFO, 'Tag deleted')
+            return HttpResponseRedirect(reverse('notes:index'))
+    else:
+        form = TagForm(instance=tag)
+    return render(request, 'notes/addtag.html', {'form':form, 'tag':tag})
