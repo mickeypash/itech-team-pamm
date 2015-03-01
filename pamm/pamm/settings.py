223090f0d6c154a200748156fbceffda7e1f0465
Mickey Pashov <pashov.m@gmail.com>
2015-03-01 13:37:26 +0000

    Added bcrypt for additional security

--- pamm/pamm/settings.py
+++ pamm/pamm/settings.py
@@ -63,6 +63,13 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
 ROOT_URLCONF = 'pamm.urls'
 
 WSGI_APPLICATION = 'pamm.wsgi.application'
