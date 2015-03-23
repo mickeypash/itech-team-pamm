9378ff75f70ef877d46eb38eb61f1bcdeabe0ff0
Mickey Pashov <pashov.m@gmail.com>
2015-03-23 00:24:41 +0000

    User auth works, Reg, Login and Logout

--- pamm/accounts/forms.py
+++ pamm/accounts/forms.py
@@ -1,21 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
-from backup.models import Note,Folder,Tag, UserProfile
-
-class NoteForm(forms.ModelForm):
-    class Meta:
-        model = Note
-        fields = ('title', 'content', 'tag')
-
-class FolderForm(forms.ModelForm):
-    class Meta:
-        model = Folder
-        fields = ('title', 'note')
-
-class TagForm(forms.ModelForm):
-    class Meta:
-        model = Tag
-        fields = ('label',)
+from notes.models import Note, Folder, Tag
+from models import UserProfile
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
