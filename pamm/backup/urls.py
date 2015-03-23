ecd57d069e858e866f82d37a52488ddfc1f2d861
Mickey Pashov <pashov.m@gmail.com>
2015-03-23 00:19:26 +0000

    Updated to Django 1.7.7

--- pamm/backup/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-# from django.views.generic import TemplateView
-from backup import views
-
-urlpatterns = patterns('',
-	url(r'^$', views.index, name='index'),
-    url(r'^register/$', views.register, name='register'),
-)
