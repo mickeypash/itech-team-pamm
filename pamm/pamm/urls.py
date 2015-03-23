fd423aedf02f57540e620d9593b06e3dd6b18d36
Mickey Pashov <pashov.m@gmail.com>
2015-03-23 00:22:19 +0000

    Semi-decent urls for project

--- pamm/pamm/urls.py
+++ pamm/pamm/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from accounts.views import login
+from accounts.views import *
+
 
 urlpatterns = patterns('',
-    url(r'^', include('backup.urls')),
+    url(r'^$', index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^notes/', include('notes.urls', namespace='notes')),
-    url(r'^login/$', login, name='home'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': 'home'}, name='logout'),
+    url(r'^register/$', register, name='register'),
+    url(r'^login/$', user_login, name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': 'index'}, name='logout'),
+    #url(r'^(?P<folder_name_slug>\w+)', folder, name='folder'),
 )
-
