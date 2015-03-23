83d6c4067420779e05701c9076ce2453128d55c2
Mickey Pashov <pashov.m@gmail.com>
2015-03-23 00:20:29 +0000

    Setting the groundworks for Testing

--- pamm/accounts/tests.py
+++ pamm/accounts/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# test registration
+def test_registration():
+    return false
+
+# test login
+def test_login():
+    return true
+
+# test logout
+def test_logout():
+    return false
+
+# test settings
+def test_settings():
+    return false
--- pamm/notes/tests.py
+++ pamm/notes/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# def test_add_note
+
+# def test_add_tag
+
+# def test_update_note (note_content)
